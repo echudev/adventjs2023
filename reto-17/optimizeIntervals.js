e2e85feb19a7606449f79b69ca59955c1a6b9e97
Ezequiel <ezequiel.maranda@gmail.com>
2024-01-03 22:02:16 -0300

    reto 17 completo

--- reto-17/optimizeIntervals.js
+++ reto-17/optimizeIntervals.js
@@ -2,7 +2,7 @@ function optimizeIntervals(intervals) {
   let response = [...intervals.sort((a, b) => a[0] - b[0])];
 
   for (let n = 0; n < response.length - 1; n++) {
-    if (response[n][1] > response[n + 1][0]) {
+    if (response[n][1] >= response[n + 1][0]) {
       if (response[n][1] > response[n + 1][1]) {
         response.splice(n + 1, 1);
         n--;
@@ -14,33 +14,5 @@ function optimizeIntervals(intervals) {
     }
   }
 
-  console.log(response);
   return response;
 }
-
-optimizeIntervals([
-  [1, 1],
-  [3, 5],
-  [3, 4],
-  [1, 2],
-  [5, 6],
-  [5, 5],
-]); // [[1, 2], [3, 4], [5, 6]]
-
-optimizeIntervals([
-  [1, 15],
-  [8, 12],
-  [4, 7],
-]); //[[1,15]]
-
-optimizeIntervals([
-  [5, 8],
-  [2, 7],
-  [3, 4],
-]); // [[2, 8]]
-
-optimizeIntervals([
-  [1, 3],
-  [8, 10],
-  [2, 6],
-]); // [[1, 6], [8, 10]]
