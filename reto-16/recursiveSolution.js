1b52482f3a11f6c361f37d22b92924b0ae9aef93
Ezequiel <ezequiel.maranda@gmail.com>
2024-01-03 22:03:23 -0300

    clean code

--- reto-16/recursiveSolution.js
+++ reto-16/recursiveSolution.js
@@ -7,19 +7,3 @@ function transformTree(tree, i = 0) {
   };
   return node;
 }
-
-// function transformTree(tree) {
-//   let max = tree.length;
-
-//   let i = (arguments[1] ??= 0);
-//   let r = +(i >= max) + +(tree[i] == null);
-//   let resp;
-
-//   [false][+(r <= 0)] ??= resp = {
-//     value: tree[i],
-//     left: transformTree(tree, i * 2 + 1),
-//     right: transformTree(tree, i * 2 + 2),
-//   };
-
-//   return (resp ??= null);
-// }
