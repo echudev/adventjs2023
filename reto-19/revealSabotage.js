8740298896cbd6e51ce1340fa4ba49a876dfe073
Ezequiel <ezequiel.maranda@gmail.com>
2024-01-05 22:03:53 -0300

    reto 19 completo

--- reto-19/revealSabotage.js
+++ reto-19/revealSabotage.js
@@ -1,3 +1,36 @@
 function revealSabotage(store) {
-  return [];
+  for (let l = 0; l < store.length; l++) {
+    for (let i = 0; i < store[l].length; i++) {
+      if (store[l][i] !== "*") {
+        let count = 0;
+        store[l - 1] && store[l - 1][i - 1] === "*" && count++;
+        store[l - 1] && store[l - 1][i] === "*" && count++;
+        store[l - 1] && store[l - 1][i + 1] === "*" && count++;
+        store[l] && store[l][i - 1] === "*" && count++;
+        store[l] && store[l][i + 1] === "*" && count++;
+        store[l + 1] && store[l + 1][i - 1] === "*" && count++;
+        store[l + 1] && store[l + 1][i] === "*" && count++;
+        store[l + 1] && store[l + 1][i + 1] === "*" && count++;
+        store[l][i] = count === 0 ? " " : String(count);
+      }
+    }
+  }
+  return store;
 }
+
+const store = [
+  ["*", " ", " ", " "],
+  [" ", " ", "*", " "],
+  [" ", " ", " ", " "],
+  ["*", " ", " ", " "],
+];
+
+console.log(revealSabotage(store));
+/* Debería mostrar:
+  [
+      ['*', '2', '1', '1'],
+      ['1', '2', '*', '1'],
+      ['1', '2', '1', '1'],
+      ['*', '1', ' ', ' ']
+  ]
+  */
